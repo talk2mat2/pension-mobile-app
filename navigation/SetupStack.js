ecc29e4667467d76e0e809d26401ce2688993237
martins chukwuma <talk2mat2@yahoo.com>
2022-03-09 03:39:52 +0100

    updated current pension data

--- navigation/SetupStack.js
+++ navigation/SetupStack.js
@@ -10,7 +10,7 @@ const Stack = createStackNavigator();
 
 function SetupStack() {
   return (
-    <Stack.Navigator initialRouteName="CPStack">
+    <Stack.Navigator initialRouteName="KYC">
       <Stack.Screen
         name="KYC"
         options={{ headerShown: false }}
